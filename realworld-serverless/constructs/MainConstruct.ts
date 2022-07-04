2c9531d8704fcf26991feda5ac70867e18a3737b
kaustubhsathefc <89383411+kaustubhsathefc@users.noreply.github.com>
2022-07-05 00:15:14 +0530

    chore: added more code

--- realworld-serverless/constructs/MainConstruct.ts
+++ realworld-serverless/constructs/MainConstruct.ts
@@ -1,38 +1,34 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import { RemovalPolicy } from 'aws-cdk-lib';
-import { join } from 'path';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
-
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda-nodejs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { RemovalPolicy } from "aws-cdk-lib";
+import { join } from "path";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
 
 export class MainConstruct extends Construct {
-    constructor(scope: Construct, id: string) {
-        super(scope, id);
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
 
-        // This is the main table of our application, it will contain all relationships and data.
-        const mainTable = new dynamodb.Table(this, 'MainTable', {
-            partitionKey: {
-                name: 'PartitionKey',
-                type: dynamodb.AttributeType.STRING
-            },
-            sortKey: {
-                name: 'SortKey',
-                type: dynamodb.AttributeType.STRING
-            },
-            removalPolicy: RemovalPolicy.DESTROY,
-            tableName: 'MainTable'
-        });
+    // This is the main table of our application, it will contain all relationships and data.
+    const mainTable = new dynamodb.Table(this, "RealWorlTable", {
+      partitionKey: {
+        name: "PK",
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: "SK",
+        type: dynamodb.AttributeType.STRING,
+      },
+      removalPolicy: RemovalPolicy.DESTROY,
+      tableName: "RealWorlTable",
+    });
 
-        const getOneLambda = new lambda.NodejsFunction(this, 'getOneLambda', {
-            entry: join(__dirname, 'lambdas', 'get-one.ts'),
-            runtime: Runtime.NODEJS_16_X
-        });
-
-        
-
-    }
+    const getOneLambda = new lambda.NodejsFunction(this, "getOneLambda", {
+      entry: join(__dirname, "lambdas", "get-one.ts"),
+      runtime: Runtime.NODEJS_16_X,
+    });
+  }
 }
--- realworld-serverless/lib/realworld-serverless-stack.ts
+++ realworld-serverless/lib/realworld-serverless-stack.ts
@@ -1,9 +1,11 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { MainConstruct } from '../constructs/MainConstruct';
 
 export class RealworldServerlessStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-        
+    
+    new MainConstruct(scope, id);
   }
 }
